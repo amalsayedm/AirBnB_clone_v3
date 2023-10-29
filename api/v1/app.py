40079b2117a64331239bcd99f472ff09c412a197
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-29 13:36:47 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """Flask web application
 """
-
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-
 @app.teardown_appcontext
 def teardown(error):
     """Clean-up method
