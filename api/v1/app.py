6e9144dc17c09891862841c28cb873131d22206e
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-29 14:42:20 +0200

    2

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,17 +3,13 @@
 """
 
 from flask import Flask, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os
+from os import getenv
 
 app = Flask(__name__)
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
@@ -30,20 +26,7 @@ def not_found(error):
     return jsonify({'error': 'Not found'}), 404
 
 
-@app.errorhandler(400)
-def not_found(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
-
-
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
