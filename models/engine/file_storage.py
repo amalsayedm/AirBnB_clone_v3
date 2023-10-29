4bcf73d7e1010e80e889bccd01f431205eea90f1
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-29 21:00:45 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,17 +71,21 @@ class FileStorage:
 
     def get(self, cls, id):
         """retrieves an object of a class with id"""
-        if cls is not None:
-            res = list(
-                filter(
-                    lambda x: type(x) is cls and x.id == id,
-                    self.__objects.values()
-                )
-            )
-            if res:
-                return res[0]
-        return None
-
+       if cls is not None and type(cls) is str and id is not None and\
+           type(id) is str and cls in classes:
+            cls = classes[cls]
+            result = self.__session.query(cls).filter(cls.id == id).first()
+            return result
+        else:
+            return None
+                   
     def count(self, cls=None):
         """retrieves the number of objects of a class or all (if cls==None)"""
-        return len(self.all(cls))
+        total = 0
+        if type(cls) == str and cls in classes:
+            cls = classes[cls]
+            total = self.__session.query(cls).count()
+        elif cls is None:
+            for cls in classes.values():
+                total += self.__session.query(cls).count()
+        return total
