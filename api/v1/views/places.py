ca2eb2cf68cdd025b6e14a48cc57d75a83e2e212
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-30 18:39:49 +0200

    Update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,7 +3,8 @@
 from flask import abort, jsonify, make_response, request
 import requests
 from api.v1.views import app_views
-from api.v1.views.amenities import Amenity
+from api.v1.views.amenities import amenities
+from api.v1.views.places_amenities import place_amenities
 from models import storage
 from models.amenity import Amenity
 from models.city import City
